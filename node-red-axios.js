c5e5cb4fdbb517c973ea21ff902bbf41eaf2fc0d
mnazirp <mnazirp@gmail.com>
2021-12-11 10:35:28 +0700

    decodeURI

--- node-red-axios.js
+++ node-red-axios.js
@@ -6,17 +6,20 @@ module.exports = function (RED) {
     var node = this;
     node.on('input', function (msg) {
       if (msg.options && msg.options.url && msg.options.method) {
+        let url = msg.options.url;
+        if(msg.options.params){
+          let p = new URLSearchParams({...msg.options.params}).toString();
+          url = `${url}?${p}`;
+        }
         axios({
           method: msg.options.method,
-          url: msg.options.url,
+          url: (msg.options.decode) ? decodeURI(url) : url,
           headers: msg.options.headers ? msg.options.headers : null,
-          params: msg.options.params ? msg.options.params : null,
           data: msg.options.data ? msg.options.data : null,
           auth: msg.options.auth ? msg.options.auth : null,
           proxy: msg.options.proxy ? msg.options.proxy : null
         }).then(res => {
           msg.payload = res.data;
-          node.warning(res)
           node.send(msg);
         }).catch(err => {
           node.error(`fetching data failed: ${err.message}`);
